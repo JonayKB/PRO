02e3379972aa2bd765d6c4b042b07ca3e876ba1d
JonayKB <jonaykb@gmail.com>
2023-09-27 14:52:30 +0100

    Terminado Ejercicio4

--- unidad-1/Tareas/tarea2/src/main/java/Ejercicio3.java
+++ unidad-1/Tareas/tarea2/src/main/java/Ejercicio3.java
@@ -7,9 +7,11 @@ public class Ejercicio3 {
 
     /**
      * Descripcion del algoritmo
+     * Desarrolle un algoritmo que realice la sumatoria de los números enteros comprendidos entre el 1 y el 10, es decir, 1 + 2 + 3 + …. + 10. Utilia un buble __for__ y un bucle __while__
      * @param args
      */
     public static void main(String[] args) {
+
 
     }
 }
---
+++ unidad-1/Tareas/tarea2/src/main/java/Ejercicio4.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+/**
+ * @author JonayKB
+ * Ejercio numero cuatro
+ */
+public class Ejercicio4 {
+    //Creamos el escaner para pedir las variables por la pantalla
+    static Scanner input = new Scanner(System.in);
+    /**
+     * Descripcion del algoritmo
+     * Desarrolle un algoritmo que permita leer un valor cualquiera N y escriba si dicho número es par o impar
+     * @param args
+     */
+    //valor es el numero que se comprobara si es par o impar
+    static int valor;
+    public static void main(String[] args) {
+        System.out.println("Introduzca el numero: ");
+        int valor = input.nextInt();
+        //Comprobamos si el resto al ser dividido entre dos es cero
+        if (valor%2==0){
+            //Si es 0, imprimimos por pantalla que es par
+            System.out.println(valor+" es par");
+        }
+        else{
+            //Sino que es impar
+            System.out.println(valor+" es impar");
+        }
+
+
+    }
+}
