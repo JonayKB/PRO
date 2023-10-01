bf3ed6ed392c5d1daf746c2dd2afdcb2b259c466
jonaykb <jonaykb@gmail.com>
2023-10-01 17:08:01 +0100

    Añadido el ejericicio8

--- unidad-1/Tareas/tarea2/src/main/java/Ejercicio8.java
+++ unidad-1/Tareas/tarea2/src/main/java/Ejercicio8.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 /**
  * @author JonayKB
@@ -8,10 +9,23 @@ public class Ejercicio8 {
      * Descripcion del algoritmo
      * Desarrolle un algoritmo que permita calcular Promedio de Notas; finaliza cuando N = 0
      * @param args
-     */
+     */ 
+        //Creamos el Scanner
+        static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
+        //Declaramos variables
+        int numero;
+        float suma = 0;
 
+        //Pedimos la cantidad de notas
+        System.out.println("¿Cuantas notas va a introducir?: ");
+        numero = input.nextInt();
 
+        for(int N = 1; N<=numero; N++){
+            System.out.println("Introduzca la nota "+N);
+            suma+=input.nextFloat();
+        }
+        System.out.println("La media es: "+suma/numero);
     }
 }
