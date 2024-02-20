190d7ac47d5b7842dda58c08d6c337a1ebe32f55
JonayKB <jonaykb@gmail.com>
2024-02-20 20:11:53 +0000

    Faltan los csv

--- unidad-5/Tareas/ExamenProJonay/src/main/java/es/ies/puerto/modelo/ficheros/abstrac/Ficheros.java
+++ unidad-5/Tareas/ExamenProJonay/src/main/java/es/ies/puerto/modelo/ficheros/abstrac/Ficheros.java
@@ -3,13 +3,16 @@ package es.ies.puerto.modelo.ficheros.abstrac;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
+
+import es.ies.puerto.modelo.abstrac.Producto;
 
 
 public abstract class Ficheros {
-    public final String RUTA_ALIMENTOS_CSV="/src/resources/alimentos.csv";
-    public final String RUTA_APARATOS_CSV="/src/resources/aparatos.csv";
-    public final String RUTA_CUIDADOSPERSONALES_CSV="/src/resources/cuidadosPersonales.csv";
-    public final String RUTA_SOUVENIRS_CSV="/src/resources/souvenirs.csv";
+    public static final String RUTA_ALIMENTOS_CSV="/src/resources/alimentos.csv";
+    public static final String RUTA_APARATOS_CSV="/src/resources/aparatos.csv";
+    public static final String RUTA_CUIDADOSPERSONALES_CSV="/src/resources/cuidadosPersonales.csv";
+    public static final String RUTA_SOUVENIRS_CSV="/src/resources/souvenirs.csv";
 
     public static boolean existe(String path){
         if (path == null || path.isEmpty()) {
@@ -18,6 +21,6 @@ public abstract class Ficheros {
         File fichero = new File(path);
         return fichero.exists() && fichero.isFile();
     }
-    public abstract void leer(String path) throws IOException;
-    public abstract void escribir();
+    public abstract Producto leer(String path, String tipo) throws IOException, ParseException;
+    public abstract boolean escribir(String path, String texto);
 }
--- unidad-5/Tareas/ExamenProJonay/src/main/java/es/ies/puerto/modelo/ficheros/csv/FileCsv.java
+++ unidad-5/Tareas/ExamenProJonay/src/main/java/es/ies/puerto/modelo/ficheros/csv/FileCsv.java
@@ -1,33 +1,88 @@
 package es.ies.puerto.modelo.ficheros.csv;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.text.ParseException;
 
+import es.ies.puerto.modelo.abstrac.Producto;
 import es.ies.puerto.modelo.ficheros.abstrac.Ficheros;
+import es.ies.puerto.modelo.productos.Alimento;
+import es.ies.puerto.modelo.productos.Aparato;
+import es.ies.puerto.modelo.productos.CuidadoPersonal;
+import es.ies.puerto.modelo.productos.Souvenir;
 
 public class FileCsv extends Ficheros{
 
 
-    public void leer(String path) throws IOException {
+    private static final int COLUMNA_NOMBRE = 0;
+    private static final int COLUMNA_PRECIO = 0;
+    private static final int COLUMNA_FENTRADA = 0;
+    private static final int COLUMNA_UDI = 0;
+    private static final int COLUMNA_FCADUCIDAD = 0;
+    private static final int COLUMNA_POPULARIDAD = 0;
+
+    public Producto leer(String path, String tipo) throws IOException, ParseException {
         if (existe(path)) {
             File fichero = new File(path);
             try ( BufferedReader br = new BufferedReader(new FileReader(fichero))) {
                 String linea;
                 while ((linea=br.readLine()) != null) {
                     String[] array = linea.split(",");
-                    System.out.println(array);
+                    switch (tipo) {
+                        case "alimento":
+                            return crearAlimento(array);
+                        case "aparato":
+                            return crearAparato(array);
+                        case "cuidadoPersonal":
+                            return crearCuidadoPersonal(array);
+                        case "souvenir":
+                            return crearSouvenir(array);
+                    
+                        default:
+                            return crearSouvenir(array);
+                    }
                 }
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
             }
         }
+        return null;
+    }
+
+    private Producto crearAlimento(String[] array) throws ParseException{
+        return new Alimento(array[COLUMNA_NOMBRE], Float.parseFloat(array[COLUMNA_PRECIO]), array[COLUMNA_FENTRADA], array[COLUMNA_UDI], array[COLUMNA_FCADUCIDAD]);
+    }
+
+    private Producto crearAparato(String[] array) {
+        return new Aparato(array[COLUMNA_NOMBRE], Float.parseFloat(array[COLUMNA_PRECIO]), array[COLUMNA_FENTRADA], array[COLUMNA_UDI]);
+    }
+
+    private Producto crearCuidadoPersonal(String[] array) {
+        return new CuidadoPersonal(array[COLUMNA_NOMBRE], Float.parseFloat(array[COLUMNA_PRECIO]), array[COLUMNA_FENTRADA], array[COLUMNA_UDI],Integer.parseInt(array[COLUMNA_POPULARIDAD]));
+    }
+
+    private Producto crearSouvenir(String[] array) {
+        return new Souvenir(array[COLUMNA_NOMBRE], Float.parseFloat(array[COLUMNA_PRECIO]), array[COLUMNA_FENTRADA], array[COLUMNA_UDI]);
     }
 
     @Override
-    public void escribir() {
-
+    public boolean escribir(String path, String texto) {
+    if (existe(path)) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+            bw.write(texto);
+            bw.newLine();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+    return false;
+    }
+
+
     
 }
