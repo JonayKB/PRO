e605c48b57798aacb67e4f246865906a709aeaee
JonayKB <jonaykb@gmail.com>
2024-04-22 16:20:09 +0100

    completado hibernate (casi)

---
+++ unidad-7/tareas/tareaMarvel JPA/src/main/java/es/ies/puerto/modelo/db/Crud.java
@@ -0,0 +1,96 @@
+package es.ies.puerto.modelo.db;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.HashSet;
+import java.util.Set;
+
+import es.ies.puerto.excepcion.UsuarioException;
+import es.ies.puerto.modelo.impl.Alias;
+import es.ies.puerto.modelo.impl.Personaje;
+//import es.ies.puerto.modelo.impl.Poder;
+import es.ies.puerto.modelo.impl.Poder;
+
+public class Crud extends Conexion{
+    public ResultSet obtener(String sql)throws UsuarioException{
+        ResultSet resultSet = null;
+        Statement statement = null;
+        try {
+            statement = getConnection().createStatement();
+            resultSet = statement.executeQuery(sql);
+        } catch (Exception e) {
+            try {
+                if (getConnection()!=null && !getConnection().isClosed()) {
+                    getConnection().close();
+                }
+                if (statement!=null && !statement.isClosed()) {
+                    statement.close();
+                }
+            } catch (Exception p) {
+                throw new UsuarioException(p.getMessage(), p);
+            }
+            throw new UsuarioException(e.getMessage(), e);  
+        }
+       
+        return resultSet;
+    }
+    public void cerrar()throws UsuarioException{
+        try {
+            if (getConnection()!=null && !getConnection().isClosed()) {
+                getConnection().close();
+            }
+        } catch (Exception p) {
+            throw new UsuarioException(p.getMessage(), p);
+        }
+    }
+    public Set<Personaje> obtenerPersonajes()throws UsuarioException{
+        ResultSet resultSet = obtener("SELECT id,genero,nombre FROM Personajes");
+        Set<Personaje> personajes = new HashSet<>();
+        try {
+            while (resultSet.next()) {
+                Integer id = resultSet.getInt("id");
+                String genero = resultSet.getString("genero");
+                String nombre = resultSet.getString("nombre");
+                Set<Poder> poderes = obtenerPoderes(id);
+                Alias alias = obtenerAlias(id);
+                personajes.add(new Personaje(id, nombre, alias, genero, poderes));
+            }
+        } catch (Exception e) {
+            throw new UsuarioException(e.getMessage(), e);
+        }
+        cerrar();
+        return personajes;
+    }
+    public Alias obtenerAlias(Integer idPersonaje) throws UsuarioException{
+        Alias alias = new Alias();
+        try {
+            ResultSet resultSet = obtener("SELECT a.id,a.alias FROM Alias as a WHERE a.personaje_id="+idPersonaje);
+            while (resultSet.next()) {
+                alias.setId(resultSet.getInt("id"));
+                alias.setAlias(resultSet.getString("alias"));
+                alias.setPersonaje(new Personaje(idPersonaje));
+                return alias;
+            }
+        } catch (Exception e) {
+            throw new UsuarioException(e.getMessage(), e);
+        }
+        cerrar();
+        return alias;
+    }
+    public Set<Poder> obtenerPoderes(Integer idPersonaje)throws UsuarioException{
+        ResultSet resultSet = obtener("SELECT p.id, p.poder FROM Poderes as p JOIN Personajes_Poderes as pp ON p.id=pp.poder_id WHERE pp.personaje_id="+idPersonaje);
+        Set<Poder> poderes = new HashSet<>();
+        try {
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String poder = resultSet.getString("poder");
+                poderes.add(new Poder(id, poder, null));
+            }
+        } catch (Exception e) {
+            throw new UsuarioException(e.getMessage(), e);
+        }
+        cerrar();
+        return poderes;
+    }
+}
+
---
+++ unidad-7/tareas/tareaMarvel JPA/src/main/java/es/ies/puerto/modelo/db/CrudHibernate.java
@@ -0,0 +1,48 @@
+package es.ies.puerto.modelo.db;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+
+import org.simpleframework.xml.core.Persist;
+
+import es.ies.puerto.modelo.impl.Personaje;
+
+public class CrudHibernate {
+    EntityManagerFactory emf;
+
+    public CrudHibernate() {
+        emf = Persistence.createEntityManagerFactory("sqlite-jpa");
+    }
+    public void actualizarPersonaje(Personaje personaje){
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.merge(personaje);
+        em.getTransaction().commit();
+    }
+    public void agregarPersonaje(Personaje personaje){
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(personaje);
+        em.getTransaction().commit();
+    }
+    public void eliminarPersonaje(Personaje personaje){
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.remove(em.contains(personaje) ? personaje : em.merge(personaje));
+        em.getTransaction().commit();
+    }
+    public Personaje buscarPersonaje(Personaje personaje){
+        EntityManager em = emf.createEntityManager();
+        return em.find(Personaje.class, personaje.getId());
+    }
+    public List<Personaje> obtenerPersonajes(){
+        EntityManager em = emf.createEntityManager();
+        return em.createQuery("SELECT p FROM PERSONAJES as p").getResultList();
+    }
+}
