e719bc7c325414a5e63fdd097686e9cbb9d8046c
JonayKB <jonaykb@gmail.com>
2024-04-26 14:57:16 +0100

    Tarea terminada

---
+++ unidad-7/tareas/tareaMarvel JPA/target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"es.ies.puerto.config"},{"l":"es.ies.puerto.excepcion"},{"l":"es.ies.puerto.modelo.db"},{"l":"es.ies.puerto.modelo.impl"},{"l":"es.ies.puerto.modelo.interfaces"}]
