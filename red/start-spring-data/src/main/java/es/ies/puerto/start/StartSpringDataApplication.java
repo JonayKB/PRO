47d69b5142135ddfba96318a984feadb072e808a
JonayKB <jonaykb@gmail.com>
2024-06-03 16:19:41 +0100

    Error

--- red/start-spring-data/src/main/java/es/ies/puerto/start/StartSpringDataApplication.java
+++ red/start-spring-data/src/main/java/es/ies/puerto/start/StartSpringDataApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("es.ies.puerto")
 public class StartSpringDataApplication {
 
 	public static void main(String[] args) {
