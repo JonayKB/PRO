007b75556558267c80f28d65fa4e9357bc0e7dd2
JonayKB <jonaykb@gmail.com>
2024-06-11 18:41:46 +0100

    COnsumo

--- red/final/web-dam/script.js
+++ red/final/web-dam/script.js
@@ -2,22 +2,34 @@ const getMobsButton = document.getElementById('getMobs');
 getMobsButton.addEventListener('click', fetchInfo);
 
 function fetchInfo() {
-    fetch('http://localhost:25565/api-rest/mob/1')
+    fetch('http://localhost:25565/api-rest/mob/')
         .then(response => response.json())
         .then(jsonObj => displayUi(jsonObj))
-        .catch(() => alert('No se encuentra la api'));
+        .catch(() => alert('La respuesta no ha sido validad'));
 }
 
-function displayUi(mob) {
-    const id = mob.id;
-
+function displayUi(mobs) {
+    const id = mobs[0].id;
+    const name = mobs[0].name;
+    const health = mobs[0].baseHealth;
+    const attack = mobs[0].baseAttack;
+    const specialAbility = mobs[0].specialAbility;
+    const dropList = mobs[0].dropList;
+    const biomesIds = mobs[0].biomesIds;
     const template = `
     <div>
-    <h1 id="head">${id}</h1>
+    <h1 id="head">ID: ${id}</h1>
+    <h3 id="attribute">NAME: ${name}</h3>
+    <h3 id="attribute">HEALTH: ${health}</h3>
+    <h3 id="attribute">ATTACK: ${attack}</h3>
+    <h3 id="attribute">SPECIAL ABILITY: ${specialAbility}</h3>
+    <h3 id="attribute">DROP LIST: ${dropList[0].name}</h3>
+    <h3 id="attribute">BIOME LIST: ${biomesIds}</h3>
+    
     </div>
     `
     console.log(template)
     // did this so I would avoid the process of having a refresh/update button
-    templateZone = document.getElementById('resultado');
+    const templateZone = document.getElementById('resultado');
     templateZone.innerHTML = template;
 }
