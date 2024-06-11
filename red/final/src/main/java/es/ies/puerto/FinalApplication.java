c8106d93e226bf49df29d711108315d7dedef192
JonayKB <jonaykb@gmail.com>
2024-06-11 02:14:09 +0100

    prueba

--- red/final/src/main/java/es/ies/puerto/FinalApplication.java
+++ red/final/src/main/java/es/ies/puerto/FinalApplication.java
@@ -2,8 +2,11 @@ package es.ies.puerto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
+
 public class FinalApplication {
 
 	public static void main(String[] args) {
